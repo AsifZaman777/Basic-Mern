4e4330794a3f090da392002c9bd9711141773ad6
AsifZaman777 <eramasif16@gmail.com>
2024-05-19 00:33:22 +0600

    conditional statement updated

---
+++ conditional-statement.js
@@ -0,0 +1,48 @@
+
+//ternary operator
+let age = 18;
+
+age > 18
+  ? console.log("You are eligible to vote")
+  : console.log("You are not eligible to vote");
+
+
+//if else
+let age1 = 18;
+if (age1 > 18) {
+  console.log("You are eligible to vote");
+} else {
+  console.log("You are not eligible to vote");
+}
+
+
+//switch case
+let day = 2;
+
+switch (day) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid day");
+    }
+    console.log("End of switch case");
+
