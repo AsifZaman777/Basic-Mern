421bc451cb83bf3075807a2d7cb78f505d1b8f8f
AsifZaman777 <eramasf16@gmail.com>
2024-05-20 00:27:27 +0600

    task1(sample) done by Eram

---
+++ week1-tasks/task1.js
@@ -0,0 +1,7 @@
+
+const givenMoney = prompt("Enter the amount of money you have given: ");
+const productCost = prompt("Enter the price of the product: ");
+
+const returnMoney = givenMoney - productCost;
+
+console.log(`The return money is: ${returnMoney}`);
